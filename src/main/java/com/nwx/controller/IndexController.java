d27fb9f098d978655c8aece4f9567b05dfd2c063
nieweixing <abraxas101@163.com>
2020-04-18 16:27:30 +0800

    springboot-learning

---
+++ src/main/java/com/nwx/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.nwx.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: nieweixing
+ * @create: 2020-04-18 16:11
+ **/
+@RestController
+public class IndexController {
+
+    private final static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("/index")
+    public String index(){
+        logger.debug("记录debug日志");
+        logger.info("访问了index方法");
+        logger.error("记录了error错误日志");
+        return "index";
+    }
+}
---
+++ src/test/java/com/nwx/SpringbootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
