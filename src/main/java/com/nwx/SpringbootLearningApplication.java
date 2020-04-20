c7d86f115f23029df6d37b9feef4ea1906feb12a
nieweixing <abraxas101@163.com>
2020-04-20 09:22:06 +0800

    springboot-learning

--- src/main/java/com/nwx/SpringbootLearningApplication.java
+++ src/main/java/com/nwx/SpringbootLearningApplication.java
@@ -1,9 +1,11 @@
 package com.nwx;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.nwx.mapper")
 public class SpringbootLearningApplication {
 
     public static void main(String[] args) {
